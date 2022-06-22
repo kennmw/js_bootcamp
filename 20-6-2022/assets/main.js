492ef51123676dc1d9fab3c621f3e787740f5300
kennmw <kennedy.w.mwinzi@gmail.com>
2022-06-22 22:04:08 +0300

    Assignment One done

--- 20-6-2022/assets/main.js
+++ 20-6-2022/assets/main.js
@@ -1,3 +1,3 @@
 function getVal(a) {
-    document.getElementById("out").innerText = "Hello,"+a+" welcome to the bootcamp";
+    document.getElementById("out").innerText = "Hello "+a+", welcome to the bootcamp";
   }
